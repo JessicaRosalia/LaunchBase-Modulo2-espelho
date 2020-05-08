72de356ea7ddc2b242e583ad293433a390c39458
Rosália Fernandes <jessicarosalia678@gmail.com>
2020-05-08 16:22:08 -0300

    Consertando o seletor da const cards no arquivo js e padronizando aspas

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 const modalOverlay = document.querySelector(".modal-overlay");
-const cards = document.querySelectorAll("card");
+const cards = document.querySelectorAll('.card');
 
 for (let card of cards) {
-    card.addEventListener("click", function () {
-        modalOverlay.classList.add("active");
+    card.addEventListener('click', function () {
+        modalOverlay.classList.add('active');
     });
 }
 
-document.querySelector(".close-modal").addEventListener("click", function () {
+document.querySelector(".close-modal").addEventListener('click', function () {
     modalOverlay.classList.remove("active");
 });
